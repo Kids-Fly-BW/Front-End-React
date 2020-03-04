99dcbaf1f5adacf16ee34e6d3ec49e6e5d94558d
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-03-04 12:14:22 -0800

    setup temp reducer state

--- kids_fly/src/index.js
+++ kids_fly/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
 import {dashboardReducer as reducer} from './reducers/dashboardReducer'
 import { createStore, applyMiddleware } from "redux"
@@ -14,12 +13,8 @@ const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store = {store}>
-<Router>
-    <App />
-    </Router>
-    </Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+document.getElementById('root'));
