b7fc2fd0d4f4d90aa7a445fc70128c63bd4eb053
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-03-04 13:00:08 -0800

    bookings state needs connection to store and backend

--- kids_fly/src/components/Booking/BookingDashboard.js
+++ kids_fly/src/components/Booking/BookingDashboard.js
@@ -1,67 +1,59 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
- const BookingDashboard = () => {
+const BookingDashboard = () => {
+  const [bookingInfo, setBookingInfo] = useState({
+    airline: "",
+    flightNumber: "",
+    numberOfTickets: ""
+  });
 
-    const [bookingInfo, setBookingInfo] = useState({
-        airline: '',
-        flightNumber: '',
-        numberOfTickets: ''
-    })
+  const handleChange = e => {
+    setBookingInfo({
+      ...bookingInfo,
+      [e.target.name]: e.target.value
+    });
+  };
 
-    const handleChange = (e) =>{
-       setBookingInfo({
-           ...bookingInfo,
-           [e.target.name]: e.target.value
-       })
-    }
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(bookingInfo)
+  };
 
+  return (
+    <div>
+      {/* this is the data from the server with other currect Bookings */}
+      <div>
+        <p>Bookings to come</p>
+        {/* Booking Form */}
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="airline">Airline Name</label>
+          <input
+            name="airline"
+            type="text"
+            value={bookingInfo.airline}
+            onChange={handleChange}
+          />
 
+          <label htmlFor="flightNumber">Flight Number</label>
+          <input
+            name="flightNumber"
+            type="text"
+            value={bookingInfo.flightNumber}
+            onChange={handleChange}
+          />
 
-//    const handleSubmit = (e) =>{
-//     e.preventDefault
-//     }
+          <label htmlFor="numberOfTickets">How many Tickets</label>
+          <input
+            name="numberOfTickets"
+            type="text"
+            value={bookingInfo.numberOfTickets}
+            onChange={handleChange}
+          />
+          <button>submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-
-    return (
-        <div>
-            {/* this is the data from the server with other currect Bookings */}
-            <div>
-            <p>Bookings to come</p>
-            {/* Booking Form */}
-            <form>
-
-
-                <label htmlFor='airline'>Airline Name</label>
-                <input
-                name='airline'
-                type='text'
-                value={bookingInfo.airline}
-                onChange={handleChange}
-                />
-
-
-                <label htmlFor='flightNumber'>Flight Number</label>
-                <input
-                name='flightNumber'
-                type='text'
-                value={bookingInfo.flightNumber}
-                onChange={handleChange}
-                />
-
-
-                <label htmlFor='numberOfTickets'>How many Tickets</label>
-                <input
-                name='numberOfTickets'
-                type='text'
-                value={bookingInfo.numberOfTickets}
-                onChange={handleChange}
-                />
-
-
-            </form>
-            </div>
-        </div>
-    )
-}
-
-export default BookingDashboard
+export default BookingDashboard;
